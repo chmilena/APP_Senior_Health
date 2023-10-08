8de0bcf29af968c87b161f5e8110ea1cdb3feada
chmilena <141787936+chmilena@users.noreply.github.com>
2023-10-08 01:16:22 -0400

    commit

--- app/src/main/java/com/example/teste/AlarmReceiver.java
+++ app/src/main/java/com/example/teste/AlarmReceiver.java
@@ -1,22 +1,15 @@
 package com.example.teste;
-import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
-
 import android.util.Log;
-
-
 import androidx.core.app.NotificationCompat;
-
-import java.util.Calendar;
 
 public class AlarmReceiver extends BroadcastReceiver {
     private static final String CHANNEL_ID = "medication_channel_id"; // ID do canal de notificação
